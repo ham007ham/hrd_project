859ff48c8250b6caf9ea97f6910ca5396401a3ab
hrd <hrd007hrd@163.com>
2017-08-17 10:10:52 +0800

    test

---
+++ src/hrdProject/hello.java
@@ -0,0 +1,14 @@
+package hrdProject;
+
+import java.util.Date;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world,hrd is greatman. superman.");
+		
+		System.out.println(new Date());
+	}
+
+}
